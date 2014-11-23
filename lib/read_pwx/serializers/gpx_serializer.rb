d2138eabd7bf7ab30c29ff155b57e5c69e12e8fc
James Badger <james@jamesbadger.ca>
2014-11-23 00:48:40 -0700

    Add very simple GPX export
    It needs to output meaningful extensions. The output at this point
    is readable as a track though.

--- lib/read_pwx/serializers/gpx_serializer.rb
+++ lib/read_pwx/serializers/gpx_serializer.rb
@@ -1,6 +1,58 @@
 module ReadPWX::Serializers
   class GPXSerializer
-    def self.dump(value)
+    class << self
+      def dump(pwx)
+        builder = Nokogiri::XML::Builder.new do |xml|
+          xml.gpx(gpx_attributes) {
+            xml.metadata {
+              xml.time Time.now.utc.iso8601
+              xml.extensions {}
+            }
+
+            pwx.workouts.each_with_index do |workout, index|
+              xml.trk { # corresponds to workout
+                xml.name workout.fingerprint
+                xml.src "#{workout.device.make} #{workout.device.model}"
+                xml.number index
+                xml.extensions {}
+
+                xml.trkseg {
+                  workout.samples.each do |sample|
+                    if !sample.lat.empty? && !sample.lon.empty?
+                      xml.trkpt(lat: sample.lat, lon: sample.lon) {
+                        # look into extensions to GPX for HRM, Cadence, etc.
+
+                        xml.ele sample.alt
+
+                        # use time, or calculate from time_offset
+                        xml.time (DateTime.strptime(workout.time, "%Y-%m-%dT%H:%I:%S") + sample.time_offset.to_i).iso8601
+
+                        xml.extensions {} # status of devices
+                      }
+                    end
+                  end
+                  xml.extensions {} # extensions to segment
+                }
+              }
+            end
+
+            xml.extensions {}
+          }
+        end
+
+        builder.doc
+      end
+
+      private
+
+      def gpx_attributes
+        {
+          "xmlns" => "http://www.topografix.com/GPX/1/1",
+          "xmlns:xsd" => "http://www.w3.org/2001/XMLSchema",
+          "creator" => "ReadPWX " + ReadPWX::VERSION,
+          "version" => "1.1"
+        }
+      end
     end
   end
 end
--- spec/read_pwx/serializers/gpx_serializer_spec.rb
+++ spec/read_pwx/serializers/gpx_serializer_spec.rb
@@ -12,4 +12,20 @@ RSpec.describe ReadPWX::Serializers::GPXSerializer do
       ReadPWX::Serializers::GPXSerializer.dump(@pwx)
     }.to_not raise_error
   end
+
+  describe "the dumped GPX document" do
+    before do
+      @gpx = ReadPWX::Serializers::GPXSerializer.dump(@pwx)
+    end
+
+    it "validates against GPX schema" do
+      schema = Nokogiri::XML::Schema(IO.read(File.join('spec', 'schemas', 'gpx.xsd')))
+      expect(schema.valid?(@gpx)).to be true
+    end
+
+    it "has no GPX schema errors" do
+      schema = Nokogiri::XML::Schema(IO.read(File.join('spec', 'schemas', 'gpx.xsd')))
+      expect(schema.validate(@gpx)).to be_empty
+    end
+  end
 end
