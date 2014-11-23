2fdf35a50fbec5e6aafce479ed4248ca0018317c
James Badger <james@jamesbadger.ca>
2014-11-22 23:51:26 -0700

    Use positional args in smaller classes

--- lib/read_pwx/pwx/athlete.rb
+++ lib/read_pwx/pwx/athlete.rb
@@ -2,9 +2,9 @@ module ReadPWX::PWX
   class Athlete
     attr_reader :name, :weight
 
-    def initialize(options = {})
-      @name = options[:name]
-      @weight = options[:weight]
+    def initialize(name: name, weight: weight)
+      @name = name
+      @weight = weight
     end
   end
 end
