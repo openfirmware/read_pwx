b6fec9b01b72897b6d0ca66c4fdc7a9f0e738960
James Badger <james@jamesbadger.ca>
2014-11-22 22:33:31 -0700

    Add specs to check for sub-instances

--- spec/read_pwx/parser_spec.rb
+++ spec/read_pwx/parser_spec.rb
@@ -67,6 +67,22 @@ RSpec.describe ReadPWX::Parser do
       it "has an array of segment instances" do
         expect(workout.segments).to be_kind_of(Array)
       end
+
+      it "has an athlete instance" do
+        expect(workout.athlete).to be_kind_of(ReadPWX::PWX::Athlete)
+      end
+
+      it "has a device instance" do
+        expect(workout.device).to be_kind_of(ReadPWX::PWX::Device)
+      end
+
+      it "has an extension instance" do
+        expect(workout.extension).to be_kind_of(ReadPWX::PWX::Extension)
+      end
+
+      it "has a summary data instance" do
+        expect(workout.summary_data).to be_kind_of(ReadPWX::PWX::SummaryData)
+      end
     end
   end
 end
