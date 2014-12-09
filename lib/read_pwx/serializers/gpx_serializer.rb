1ee14bbd28d63ce0c0df6b3dfc92e176552c59ae
James Badger <james@jamesbadger.ca>
2014-12-09 01:36:19 -0700

    Extract parts of serializer to methods
    Trying to avoid one large method for serialization.

--- lib/read_pwx/serializers/gpx_serializer.rb
+++ lib/read_pwx/serializers/gpx_serializer.rb
@@ -2,64 +2,15 @@ module ReadPWX::Serializers
   class GPXSerializer
     class << self
       def dump(pwx)
-        builder = Nokogiri::XML::Builder.new(encoding: "UTF-8") do |xml|
+        Nokogiri::XML::Builder.new(encoding: "UTF-8") { |xml|
           xml.gpx(gpx_attributes) {
-            xml.metadata {
-              xml.author {
-                xml.name "ReadPWX"
-                xml.link(href: "https://github.com/openfirmware/read_pwx") {
-                  xml.text_ "Github: openfirmware/read_pwx"
-                  xml.type_ "text/html"
-                }
-              }
-              xml.time Time.now.utc.iso8601
-            }
+            metadata(xml)
 
             pwx.workouts.each_with_index do |workout, index|
-              xml.trk { # corresponds to workout
-                xml.name workout.fingerprint
-                xml.src "#{workout.device.make} #{workout.device.model}"
-                xml.number index
-                xml.type_ workout.sport_type
-                xml.extensions {
-                  xml['gpxdata'].run {
-                    xml['gpxdata'].sport gpx_sport_enum(workout.sport_type)
-                    xml['gpxdata'].laps {
-                      workout.segments.each_with_index do |segment, index|
-                        xml['gpxdata'].lap {
-                          xml['gpxdata'].index index
-                          xml['gpxdata'].elapsedTime segment.summary_data.duration
-                          xml['gpxdata'].distance segment.summary_data.dist
-                        }
-                      end
-                    }
-                  }
-                }
-
-                xml.trkseg {
-                  workout.samples.each do |sample|
-                    if !sample.lat.empty? && !sample.lon.empty?
-                      xml.trkpt(lat: sample.lat, lon: sample.lon) {
-                        xml.ele sample.alt
-                        xml.time sample.time
-
-                        xml.extensions {
-                          xml['gpxdata'].cadence sample.cad unless sample.cad.empty?
-                          xml['gpxdata'].distance sample.dist unless sample.dist.empty?
-                          xml['gpxdata'].hr sample.hr unless sample.hr.empty?
-                          xml['gpxdata'].power sample.pwr unless sample.pwr.empty?
-                          xml['gpxdata'].temp sample.temp unless sample.temp.empty?
-                        }
-                      }
-                    end
-                  end
-                }
-              }
+              trk(xml, workout, index)
             end
           }
-        end
-
-        builder.doc
+        }.doc
       end
 
       private
@@ -80,6 +31,76 @@ module ReadPWX::Serializers
           when "Run" then 'run'
           else 'other'
         end
+      end
+
+      def author(xml)
+        xml.author {
+          xml.name "ReadPWX"
+          xml.link(href: "https://github.com/openfirmware/read_pwx") {
+            xml.text_ "Github: openfirmware/read_pwx"
+            xml.type_ "text/html"
+          }
+        }
+      end
+
+      def metadata(xml)
+        xml.metadata {
+          author(xml)
+          xml.time Time.now.utc.iso8601
+        }
+      end
+
+      def trk(xml, workout, index)
+        xml.trk {
+          xml.name workout.fingerprint
+          xml.src "#{workout.device.make} #{workout.device.model}"
+          xml.number index
+          xml.type_ workout.sport_type
+          trk_extensions(xml, workout)
+
+          xml.trkseg {
+            workout.samples.each do |sample|
+              trkpt(xml, sample)
+            end
+          }
+        }
+      end
+
+      def trk_extensions(xml, workout)
+        xml.extensions {
+          xml['gpxdata'].run {
+            xml['gpxdata'].sport gpx_sport_enum(workout.sport_type)
+            xml['gpxdata'].laps {
+              workout.segments.each_with_index do |segment, index|
+                xml['gpxdata'].lap {
+                  xml['gpxdata'].index index
+                  xml['gpxdata'].elapsedTime segment.summary_data.duration
+                  xml['gpxdata'].distance segment.summary_data.dist
+                }
+              end
+            }
+          }
+        }
+      end
+
+      def trkpt(xml, sample)
+        if !sample.lat.empty? && !sample.lon.empty?
+          xml.trkpt(lat: sample.lat, lon: sample.lon) {
+            xml.ele sample.alt
+            xml.time sample.time
+            trkpt_extensions(xml, sample)
+          }
+        end
+      end
+
+      def trkpt_extensions(xml, sample)
+        xml.extensions {
+          xml['gpxdata'].cadence sample.cad unless sample.cad.empty?
+          xml['gpxdata'].distance sample.dist unless sample.dist.empty?
+          xml['gpxdata'].hr sample.hr unless sample.hr.empty?
+          xml['gpxdata'].power sample.pwr unless sample.pwr.empty?
+          xml['gpxdata'].temp sample.temp unless sample.temp.empty?
+        }
       end
     end
   end
